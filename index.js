9f42d31b1858667358c487c699aae7e2891f9e7c
A-312 <A-312@users.noreply.github.com>
2019-08-09 13:45:08 +0200

    show error

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ module.exports = function (options) {
     if (res.error) {
       res.error.filePath = file.path; 
       res.error.fileContent = (typeof build === "string") ? build : build[res.error.filename]; 
+      printError(res.error);
       return next(new PluginError('gulp-terser', res.error));
     }
 
@@ -51,3 +52,34 @@ module.exports = function (options) {
     return next(null, file);
   });
 };
+
+function alignLine(num, text, longer, maxsize) {
+	let maxlength = process.stdout.columns - 1;
+	num = num + Array(longer - (num + "").length).join(" ");
+	maxlength -= num.length + 3;
+	console.log("\033[36m" + num + " |\033[00m " + text.slice(0, (maxsize < maxlength) ? maxlength : maxsize));
+}
+
+function printError(ev) {
+	if (!ev.fileContent || (ev.line === undefined && ev.col === undefined))
+		return console.error(ev.stack || ev);
+
+	const fileContent = ev.fileContent;
+	const lines = fileContent.replace(/\t/g, "    ").split("\n"),
+		more = (ev.line + " ").length,
+		col = ev.col + 1,
+		pos = (more + 2) + fileContent.split("\n")[ev.line - 1].replace(/[^\t]/g, "").length * 3 + parseInt(col);
+
+	console.log(`\nError with ${ev.plugin} :`);
+	for (let i = ev.line - 5; i < ev.line; i++) {
+		if (lines[i]) {
+			alignLine(i + 1, lines[i], more, pos);
+		}
+	}
+
+	console.log(Array(pos).join(" ") + "\033[91m^\033[00m");
+	console.log("(" + ev.name + ") " + ev.message + " (line : \033[96m" + ev.line + "\033[00m, col : \033[96m" + col + "\033[00m).");
+	console.log(`in : ${ev.filePath}\n`);
+
+	return this.emit('end');
+}
